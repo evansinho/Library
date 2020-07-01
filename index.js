cba83246479e7acd42862c5a895aea42caaff9b5
evansinho <igiri.evanson@gmail.com>
2020-07-01 09:21:18 +0100

    created starter files

---
+++ index.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+class Book {
+  constructor(author, tittle, pages, read) {
+    this.author = author;
+    this.tittle = tittle;
+    this.pages  = pages;
+    this.read = read;
+  }
+}
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
